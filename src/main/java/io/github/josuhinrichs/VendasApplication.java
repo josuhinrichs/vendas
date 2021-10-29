bb3c8783674690404f196cbfaf12436e7de67019
josuhinrichs <josue2001marinho@gmail.com>
2021-10-29 10:27:15 -0300

    Adaptação para JPA

--- src/main/java/io/github/josuhinrichs/VendasApplication.java
+++ src/main/java/io/github/josuhinrichs/VendasApplication.java
@@ -32,10 +32,10 @@ public class VendasApplication {
             System.out.println("Buscando Clientes");
             clientes.buscarPorNome("Ni").forEach(System.out::println);
 
-            /*System.out.println("Deletando Clientes");
+            System.out.println("Deletando Clientes");
             clientes.obterTodos().forEach(c->{
                 clientes.deletar(c);
-            });*/
+            });
 
             todosClientes = clientes.obterTodos();
             if(todosClientes.isEmpty()){
